1f06e93d5a4d2eaffe74efccd289aac1e3524434
Koushikraj Rajendran <koushikraj2@gmail.com>
2019-07-06 13:04:21 +0100

    NFC Detection

--- app/src/main/java/com/example/dbs_nfc/MainActivity.java
+++ app/src/main/java/com/example/dbs_nfc/MainActivity.java
@@ -3,26 +3,51 @@ package com.example.dbs_nfc;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.http.SslError;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private ProgressDialog progDailog;
     Activity activity;
+    private NfcAdapter adapter = null;
     private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        adapter = NfcAdapter.getDefaultAdapter(this);
     }
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d("onNewIntent", "Discovered tag with intent " + intent);
+
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        String tagId = Utils.bytesToHex(tag.getId());
+ mTextView=findViewById(R.id.message);
+        mTextView.setText("Tag " + tagId);
+    }
+
 
     public void openWeb(View view) {
         final WebView lib_web=findViewById(R.id.webView);
