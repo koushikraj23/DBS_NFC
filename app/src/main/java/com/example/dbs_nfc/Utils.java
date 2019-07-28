b5d766b8310911d12f8d82817b5413b7b4e63c1d
Koushikraj Rajendran <koushikraj2@gmail.com>
2019-07-29 00:39:43 +0100

    to read data from firebase when a tag is detected

--- app/src/main/java/com/example/dbs_nfc/Utils.java
+++ app/src/main/java/com/example/dbs_nfc/Utils.java
@@ -24,6 +24,9 @@ final class Utils {
             hexChars[j * 2] = hexArray[v >>> 4];
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
+        String tag="0x" + new String(hexChars);
+        dbHelper dbase=new dbHelper();
+        dbase.readData(tag);
         return "0x" + new String(hexChars);
     }
 
@@ -61,8 +64,10 @@ final class Utils {
 
 
     public static  void storeID(Context context, String tagId, String name,String pswd){
-        dbHelper dbase;
-         dbase=new dbHelper();
+        dbHelper dbase=new dbHelper();
         dbase.insert(tagId,name,pswd);
     }
+
+
+
 }
--- app/src/main/java/com/example/dbs_nfc/dbHelper.java
+++ app/src/main/java/com/example/dbs_nfc/dbHelper.java
@@ -9,10 +9,14 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -56,6 +60,47 @@ public class dbHelper{
         user.put("cardId", cardID);
         user.put("Pswd", pswd);
 
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
+    }
+
+
+    public void readData( String cardID){
+
+
+        Map<String, Object> user = new HashMap<>();
+
+        user.put("cardId", cardID);
+        db.collection("users")
+                .whereEqualTo("cardId", cardID)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 // Add a new document with a generated ID
         db.collection("users")
                 .add(user)
